47bcb41afb58c456601464d53139df4e497367b7
lsurma <kontakt@lsurma.pl>
2025-12-06 16:51:16 +0100

    feat: Add initial migration for ProjectInstances and TranslationsSets tables

--- DataManager.Application.Core/Data/Migrations/20251206154502_init.Designer.cs
+++ DataManager.Application.Core/Data/Migrations/20251206154502_init.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataManager.Application.Core.Data.Migrations
 {
     [DbContext(typeof(DataManagerDbContext))]
-    [Migration("20251130113602_init")]
+    [Migration("20251206154502_init")]
     partial class init
     {
         /// <inheritdoc />
@@ -19,69 +19,6 @@ namespace DataManager.Application.Core.Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.0");
-
-            modelBuilder.Entity("DataManager.Application.Core.Modules.DataSet.DataSet", b =>
-                {
-                    b.Property<Guid>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("TEXT");
-
-                    b.Property<string>("AllowedIdentityIds")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
-
-                    b.Property<long>("CreatedAt")
-                        .HasColumnType("INTEGER");
-
-                    b.Property<string>("CreatedBy")
-                        .IsRequired()
-                        .HasMaxLength(256)
-                        .HasColumnType("TEXT");
-
-                    b.Property<string>("Description")
-                        .HasMaxLength(1000)
-                        .HasColumnType("TEXT");
-
-                    b.Property<string>("Name")
-                        .IsRequired()
-                        .HasMaxLength(200)
-                        .HasColumnType("TEXT");
-
-                    b.Property<string>("Notes")
-                        .HasColumnType("TEXT");
-
-                    b.Property<long?>("UpdatedAt")
-                        .HasColumnType("INTEGER");
-
-                    b.Property<string>("UpdatedBy")
-                        .HasMaxLength(256)
-                        .HasColumnType("TEXT");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("DataSets");
-                });
-
-            modelBuilder.Entity("DataManager.Application.Core.Modules.DataSet.DataSetInclude", b =>
-                {
-                    b.Property<Guid>("ParentDataSetId")
-                        .HasColumnType("TEXT");
-
-                    b.Property<Guid>("IncludedDataSetId")
-                        .HasColumnType("TEXT");
-
-                    b.Property<long>("CreatedAt")
-                        .HasColumnType("INTEGER");
-
-                    b.HasKey("ParentDataSetId", "IncludedDataSetId");
-
-                    b.HasIndex("IncludedDataSetId");
-
-                    b.HasIndex("ParentDataSetId", "IncludedDataSetId")
-                        .IsUnique();
-
-                    b.ToTable("DataSetInclude");
-                });
 
             modelBuilder.Entity("DataManager.Application.Core.Modules.ProjectInstance.ProjectInstance", b =>
                 {
@@ -155,9 +92,6 @@ namespace DataManager.Application.Core.Data.Migrations
                         .HasMaxLength(10)
                         .HasColumnType("TEXT");
 
-                    b.Property<Guid?>("DataSetId")
-                        .HasColumnType("TEXT");
-
                     b.Property<string>("InternalGroupName1")
                         .HasMaxLength(200)
                         .HasColumnType("TEXT");
@@ -166,7 +100,25 @@ namespace DataManager.Application.Core.Data.Migrations
                         .HasMaxLength(200)
                         .HasColumnType("TEXT");
 
+                    b.Property<bool>("IsCurrentVersion")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER")
+                        .HasDefaultValue(true);
+
+                    b.Property<bool>("IsDraftVersion")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER")
+                        .HasDefaultValue(false);
+
+                    b.Property<bool>("IsOldVersion")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER")
+                        .HasDefaultValue(false);
+
                     b.Property<Guid?>("LayoutId")
+                        .HasColumnType("TEXT");
+
+                    b.Property<Guid?>("OriginalTranslationId")
                         .HasColumnType("TEXT");
 
                     b.Property<string>("ResourceName")
@@ -177,9 +129,18 @@ namespace DataManager.Application.Core.Data.Migrations
                     b.Property<Guid?>("SourceId")
                         .HasColumnType("TEXT");
 
+                    b.Property<Guid?>("SourceTranslationId")
+                        .HasColumnType("TEXT");
+
+                    b.Property<DateTimeOffset?>("SourceTranslationLastSyncedAt")
+                        .HasColumnType("TEXT");
+
                     b.Property<string>("TranslationName")
                         .IsRequired()
                         .HasMaxLength(200)
+                        .HasColumnType("TEXT");
+
+                    b.Property<Guid?>("TranslationsSetId")
                         .HasColumnType("TEXT");
 
                     b.Property<long?>("UpdatedAt")
@@ -193,34 +154,87 @@ namespace DataManager.Application.Core.Data.Migrations
 
                     b.HasIndex("CultureName");
 
-                    b.HasIndex("DataSetId");
-
                     b.HasIndex("LayoutId");
 
+                    b.HasIndex("OriginalTranslationId");
+
                     b.HasIndex("SourceId");
+
+                    b.HasIndex("SourceTranslationId");
+
+                    b.HasIndex("TranslationsSetId");
+
+                    b.HasIndex("IsCurrentVersion", "IsDraftVersion", "IsOldVersion");
 
                     b.HasIndex("InternalGroupName1", "InternalGroupName2", "ResourceName", "CultureName");
 
                     b.ToTable("Translations");
                 });
 
-            modelBuilder.Entity("DataManager.Application.Core.Modules.DataSet.DataSetInclude", b =>
+            modelBuilder.Entity("DataManager.Application.Core.Modules.TranslationsSet.TranslationsSet", b =>
                 {
-                    b.HasOne("DataManager.Application.Core.Modules.DataSet.DataSet", "IncludedDataSet")
-                        .WithMany("IncludedIn")
-                        .HasForeignKey("IncludedDataSetId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("TEXT");
 
-                    b.HasOne("DataManager.Application.Core.Modules.DataSet.DataSet", "ParentDataSet")
-                        .WithMany("Includes")
-                        .HasForeignKey("ParentDataSetId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
+                    b.Property<string>("AllowedIdentityIds")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
 
-                    b.Navigation("IncludedDataSet");
+                    b.Property<string>("AvailableCultures")
+                        .HasColumnType("TEXT");
 
-                    b.Navigation("ParentDataSet");
+                    b.Property<long>("CreatedAt")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("CreatedBy")
+                        .IsRequired()
+                        .HasMaxLength(256)
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Description")
+                        .HasMaxLength(1000)
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(200)
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Notes")
+                        .HasColumnType("TEXT");
+
+                    b.Property<long?>("UpdatedAt")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("UpdatedBy")
+                        .HasMaxLength(256)
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("TranslationsSets", (string)null);
+                });
+
+            modelBuilder.Entity("DataManager.Application.Core.Modules.TranslationsSet.TranslationsSetInclude", b =>
+                {
+                    b.Property<Guid>("ParentTranslationsSetId")
+                        .HasColumnType("TEXT");
+
+                    b.Property<Guid>("IncludedTranslationsSetId")
+                        .HasColumnType("TEXT");
+
+                    b.Property<long>("CreatedAt")
+                        .HasColumnType("INTEGER");
+
+                    b.HasKey("ParentTranslationsSetId", "IncludedTranslationsSetId");
+
+                    b.HasIndex("IncludedTranslationsSetId");
+
+                    b.HasIndex("ParentTranslationsSetId", "IncludedTranslationsSetId")
+                        .IsUnique();
+
+                    b.ToTable("TranslationsSetsIncludes", (string)null);
                 });
 
             modelBuilder.Entity("DataManager.Application.Core.Modules.ProjectInstance.ProjectInstance", b =>
@@ -235,14 +249,14 @@ namespace DataManager.Application.Core.Data.Migrations
 
             modelBuilder.Entity("DataManager.Application.Core.Modules.Translations.Translation", b =>
                 {
-                    b.HasOne("DataManager.Application.Core.Modules.DataSet.DataSet", "DataSet")
-                        .WithMany()
-                        .HasForeignKey("DataSetId")
-                        .OnDelete(DeleteBehavior.SetNull);
-
                     b.HasOne("DataManager.Application.Core.Modules.Translations.Translation", "Layout")
                         .WithMany()
                         .HasForeignKey("LayoutId")
+                        .OnDelete(DeleteBehavior.SetNull);
+
+                    b.HasOne("DataManager.Application.Core.Modules.Translations.Translation", "OriginalTranslation")
+                        .WithMany()
+                        .HasForeignKey("OriginalTranslationId")
                         .OnDelete(DeleteBehavior.SetNull);
 
                     b.HasOne("DataManager.Application.Core.Modules.Translations.Translation", "Source")
@@ -250,23 +264,56 @@ namespace DataManager.Application.Core.Data.Migrations
                         .HasForeignKey("SourceId")
                         .OnDelete(DeleteBehavior.SetNull);
 
-                    b.Navigation("DataSet");
+                    b.HasOne("DataManager.Application.Core.Modules.Translations.Translation", "SourceTranslation")
+                        .WithMany()
+                        .HasForeignKey("SourceTranslationId")
+                        .OnDelete(DeleteBehavior.SetNull);
+
+                    b.HasOne("DataManager.Application.Core.Modules.TranslationsSet.TranslationsSet", "TranslationsSet")
+                        .WithMany()
+                        .HasForeignKey("TranslationsSetId")
+                        .OnDelete(DeleteBehavior.SetNull);
 
                     b.Navigation("Layout");
 
+                    b.Navigation("OriginalTranslation");
+
                     b.Navigation("Source");
+
+                    b.Navigation("SourceTranslation");
+
+                    b.Navigation("TranslationsSet");
                 });
 
-            modelBuilder.Entity("DataManager.Application.Core.Modules.DataSet.DataSet", b =>
+            modelBuilder.Entity("DataManager.Application.Core.Modules.TranslationsSet.TranslationsSetInclude", b =>
                 {
-                    b.Navigation("IncludedIn");
+                    b.HasOne("DataManager.Application.Core.Modules.TranslationsSet.TranslationsSet", "IncludedTranslationsSet")
+                        .WithMany("IncludedIn")
+                        .HasForeignKey("IncludedTranslationsSetId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
 
-                    b.Navigation("Includes");
+                    b.HasOne("DataManager.Application.Core.Modules.TranslationsSet.TranslationsSet", "ParentTranslationsSet")
+                        .WithMany("Includes")
+                        .HasForeignKey("ParentTranslationsSetId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("IncludedTranslationsSet");
+
+                    b.Navigation("ParentTranslationsSet");
                 });
 
             modelBuilder.Entity("DataManager.Application.Core.Modules.ProjectInstance.ProjectInstance", b =>
                 {
                     b.Navigation("ChildProjects");
+                });
+
+            modelBuilder.Entity("DataManager.Application.Core.Modules.TranslationsSet.TranslationsSet", b =>
+                {
+                    b.Navigation("IncludedIn");
+
+                    b.Navigation("Includes");
                 });
 #pragma warning restore 612, 618
         }
---
+++ DataManager.Application.Core/Data/Migrations/20251206154502_init.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataManager.Application.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProjectInstances",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: true),
+                    MainHost = table.Column<string>(type: "TEXT", maxLength: 500, nullable: true),
+                    Notes = table.Column<string>(type: "TEXT", nullable: true),
+                    ParentProjectId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    CreatedAt = table.Column<long>(type: "INTEGER", nullable: false),
+                    UpdatedAt = table.Column<long>(type: "INTEGER", nullable: true),
+                    CreatedBy = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                    UpdatedBy = table.Column<string>(type: "TEXT", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectInstances", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProjectInstances_ProjectInstances_ParentProjectId",
+                        column: x => x.ParentProjectId,
+                        principalTable: "ProjectInstances",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TranslationsSets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: true),
+                    Notes = table.Column<string>(type: "TEXT", nullable: true),
+                    AllowedIdentityIds = table.Column<string>(type: "TEXT", nullable: false),
+                    AvailableCultures = table.Column<string>(type: "TEXT", nullable: true),
+                    CreatedAt = table.Column<long>(type: "INTEGER", nullable: false),
+                    UpdatedAt = table.Column<long>(type: "INTEGER", nullable: true),
+                    CreatedBy = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                    UpdatedBy = table.Column<string>(type: "TEXT", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TranslationsSets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Translations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    InternalGroupName1 = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true),
+                    InternalGroupName2 = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true),
+                    ResourceName = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    TranslationName = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    CultureName = table.Column<string>(type: "TEXT", maxLength: 10, nullable: true),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    ContentTemplate = table.Column<string>(type: "TEXT", nullable: true),
+                    TranslationsSetId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    SourceTranslationId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    SourceTranslationLastSyncedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    LayoutId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    SourceId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    IsCurrentVersion = table.Column<bool>(type: "INTEGER", nullable: false, defaultValue: true),
+                    IsDraftVersion = table.Column<bool>(type: "INTEGER", nullable: false, defaultValue: false),
+                    IsOldVersion = table.Column<bool>(type: "INTEGER", nullable: false, defaultValue: false),
+                    OriginalTranslationId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    CreatedAt = table.Column<long>(type: "INTEGER", nullable: false),
+                    UpdatedAt = table.Column<long>(type: "INTEGER", nullable: true),
+                    CreatedBy = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                    UpdatedBy = table.Column<string>(type: "TEXT", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Translations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Translations_TranslationsSets_TranslationsSetId",
+                        column: x => x.TranslationsSetId,
+                        principalTable: "TranslationsSets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Translations_Translations_LayoutId",
+                        column: x => x.LayoutId,
+                        principalTable: "Translations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Translations_Translations_OriginalTranslationId",
+                        column: x => x.OriginalTranslationId,
+                        principalTable: "Translations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Translations_Translations_SourceId",
+                        column: x => x.SourceId,
+                        principalTable: "Translations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Translations_Translations_SourceTranslationId",
+                        column: x => x.SourceTranslationId,
+                        principalTable: "Translations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TranslationsSetsIncludes",
+                columns: table => new
+                {
+                    ParentTranslationsSetId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    IncludedTranslationsSetId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TranslationsSetsIncludes", x => new { x.ParentTranslationsSetId, x.IncludedTranslationsSetId });
+                    table.ForeignKey(
+                        name: "FK_TranslationsSetsIncludes_TranslationsSets_IncludedTranslationsSetId",
+                        column: x => x.IncludedTranslationsSetId,
+                        principalTable: "TranslationsSets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TranslationsSetsIncludes_TranslationsSets_ParentTranslationsSetId",
+                        column: x => x.ParentTranslationsSetId,
+                        principalTable: "TranslationsSets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectInstances_ParentProjectId",
+                table: "ProjectInstances",
+                column: "ParentProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Translations_CultureName",
+                table: "Translations",
+                column: "CultureName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Translations_InternalGroupName1_InternalGroupName2_ResourceName_CultureName",
+                table: "Translations",
+                columns: new[] { "InternalGroupName1", "InternalGroupName2", "ResourceName", "CultureName" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Translations_IsCurrentVersion_IsDraftVersion_IsOldVersion",
+                table: "Translations",
+                columns: new[] { "IsCurrentVersion", "IsDraftVersion", "IsOldVersion" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Translations_LayoutId",
+                table: "Translations",
+                column: "LayoutId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Translations_OriginalTranslationId",
+                table: "Translations",
+                column: "OriginalTranslationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Translations_SourceId",
+                table: "Translations",
+                column: "SourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Translations_SourceTranslationId",
+                table: "Translations",
+                column: "SourceTranslationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Translations_TranslationsSetId",
+                table: "Translations",
+                column: "TranslationsSetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TranslationsSetsIncludes_IncludedTranslationsSetId",
+                table: "TranslationsSetsIncludes",
+                column: "IncludedTranslationsSetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TranslationsSetsIncludes_ParentTranslationsSetId_IncludedTranslationsSetId",
+                table: "TranslationsSetsIncludes",
+                columns: new[] { "ParentTranslationsSetId", "IncludedTranslationsSetId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProjectInstances");
+
+            migrationBuilder.DropTable(
+                name: "Translations");
+
+            migrationBuilder.DropTable(
+                name: "TranslationsSetsIncludes");
+
+            migrationBuilder.DropTable(
+                name: "TranslationsSets");
+        }
+    }
+}
